bacdb6c199e54222e7f99140c8d2a39478bbc23b
VishwathRam <83744078+VishwathRam@users.noreply.github.com>
2022-10-04 15:37:58 +0530

    Add files via upload

---
+++ server1.py
@@ -0,0 +1,407 @@
+import os
+import math
+import matplotlib.pyplot as plt
+import tensorflow as tf
+import tensorflow_datasets as tfds
+
+import flwr as fl
+import sys
+import numpy as np
+
+from tensorflow import keras
+from tensorflow.keras import layers
+
+from typing import Tuple,Dict,Optional
+
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
+
+
+# Dataset hyperparameters
+unlabeled_dataset_size = 5000
+labeled_dataset_size = 50
+image_size = 150
+image_channels = 3
+
+# Algorithm hyperparameters
+num_epochs = 1 #100
+batch_size = 100  #100 # Corresponds to 200 steps per epoch
+width = 128
+temperature = 0.1
+# Stronger augmentations for contrastive, weaker ones for supervised training
+contrastive_augmentation = {"min_area": 0.25, "brightness": 0.6, "jitter": 0.2}
+classification_augmentation = {"min_area": 0.75, "brightness": 0.3, "jitter": 0.1}
+
+def prepare_dataset():
+    # Labeled and unlabeled samples are loaded synchronously
+    # with batch sizes selected accordingly
+    steps_per_epoch = (unlabeled_dataset_size + labeled_dataset_size) // batch_size
+    unlabeled_batch_size = unlabeled_dataset_size // steps_per_epoch
+    labeled_batch_size = labeled_dataset_size // steps_per_epoch
+    print(
+        f"batch size is {unlabeled_batch_size} (unlabeled) + {labeled_batch_size} (labeled)"
+    )
+
+    unlabeled_train_dataset = (
+        tfds.load("colorectal_histology", split="train", as_supervised=True, shuffle_files=True)
+            #.batch(unlabeled_batch_size)
+    )
+    #unlabeled_train_dataset = unlabeled_train_dataset.filter(lambda img, label: (label == 0 or label == 1))
+    unlabeled_train_dataset = unlabeled_train_dataset.batch(unlabeled_batch_size)
+
+    labeled_train_dataset = (
+        tfds.load("colorectal_histology", split="train[1%:]", as_supervised=True, shuffle_files=True)#.batch(labeled_batch_size)
+    )
+    #labeled_train_dataset = labeled_train_dataset.filter(lambda img, label: (label == 0 or label == 1))
+    labeled_train_dataset = labeled_train_dataset.batch(labeled_batch_size)
+
+    test_dataset = (
+        tfds.load("colorectal_histology", split="train[:1%]", as_supervised=True, shuffle_files=True)#.batch(labeled_batch_size)
+    )
+    #test_dataset = test_dataset.filter(lambda img, label: (label == 0 or label == 1))
+    test_dataset = test_dataset.batch(labeled_batch_size)
+
+    train_dataset = tf.data.Dataset.zip(
+        (unlabeled_train_dataset, labeled_train_dataset)
+    )
+    return train_dataset, labeled_train_dataset, test_dataset
+
+# Load STL10 dataset
+train_dataset, labeled_train_dataset, test_dataset = prepare_dataset()
+
+####################################################################################################################
+class RandomColorAffine(layers.Layer):
+    def __init__(self, brightness=0, jitter=0, **kwargs):
+        super().__init__(**kwargs)
+
+        self.brightness = brightness
+        self.jitter = jitter
+
+    def get_config(self):
+        config = super().get_config()
+        config.update({"brightness": self.brightness, "jitter": self.jitter})
+        return config
+
+    def call(self, images, training=True):
+        if training:
+            batch_size = tf.shape(images)[0]
+
+            # Same for all colors
+            brightness_scales = 1 + tf.random.uniform(
+                (batch_size, 1, 1, 1), minval=-self.brightness, maxval=self.brightness
+            )
+            # Different for all colors
+            jitter_matrices = tf.random.uniform(
+                (batch_size, 1, 3, 3), minval=-self.jitter, maxval=self.jitter
+            )
+
+            color_transforms = (
+                tf.eye(3, batch_shape=[batch_size, 1]) * brightness_scales
+                + jitter_matrices
+            )
+            images = tf.clip_by_value(tf.matmul(images, color_transforms), 0, 1)
+        return images
+
+# Image augmentation module
+def get_augmenter(min_area, brightness, jitter):
+    zoom_factor = 1.0 - math.sqrt(min_area)
+    return keras.Sequential(
+        [
+            keras.Input(shape=(image_size, image_size, image_channels)),
+            layers.Rescaling(1 / 255),
+            layers.RandomFlip("horizontal"),
+            layers.RandomTranslation(zoom_factor / 2, zoom_factor / 2),
+            layers.RandomZoom((-zoom_factor, 0.0), (-zoom_factor, 0.0)),
+            RandomColorAffine(brightness, jitter),
+        ]
+    )
+
+
+def visualize_augmentations(num_images):
+    # Sample a batch from a dataset
+    images = next(iter(train_dataset))[0][0][:num_images]
+    # Apply augmentations
+    augmented_images = zip(
+        images,
+        get_augmenter(**classification_augmentation)(images),
+        get_augmenter(**contrastive_augmentation)(images),
+        get_augmenter(**contrastive_augmentation)(images),
+    )
+
+visualize_augmentations(num_images=8)
+
+###############################################################################################################
+# Define the encoder architecture
+def get_encoder():
+    return keras.Sequential(
+        [
+            keras.Input(shape=(image_size, image_size, image_channels)),
+            layers.Conv2D(width, kernel_size=3, strides=2, activation="relu"),
+            layers.Conv2D(width, kernel_size=3, strides=2, activation="relu"),
+            layers.Conv2D(width, kernel_size=3, strides=2, activation="relu"),
+            layers.Conv2D(width, kernel_size=3, strides=2, activation="relu"),
+            layers.Flatten(),
+            layers.Dense(width, activation="relu"),
+        ],
+        name="encoder",
+    )
+
+#################################################################################################################
+
+# Baseline supervised training with random initialization
+baseline_model = keras.Sequential(
+    [
+        keras.Input(shape=(image_size, image_size, image_channels)),
+        get_augmenter(**classification_augmentation),
+        get_encoder(),
+        layers.Dense(10),
+    ],
+    name="baseline_model",
+)
+baseline_model.compile(
+    optimizer=keras.optimizers.Adam(),
+    loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+    metrics=[keras.metrics.SparseCategoricalAccuracy(name="acc")],
+)
+
+baseline_history = baseline_model.fit(
+    labeled_train_dataset, epochs=num_epochs, validation_data=test_dataset
+)
+
+print(
+    "Maximal validation accuracy: {:.2f}%".format(
+        max(baseline_history.history["val_acc"]) * 100
+    )
+)
+
+#################################################################################################################
+
+# Self-supervised model for contrastive pretraining
+# Define the contrastive model with model-subclassing
+class ContrastiveModel(keras.Model):
+    def __init__(self):
+        super().__init__()
+
+        self.temperature = temperature
+        self.contrastive_augmenter = get_augmenter(**contrastive_augmentation)
+        self.classification_augmenter = get_augmenter(**classification_augmentation)
+        self.encoder = get_encoder()
+        # Non-linear MLP as projection head
+        self.projection_head = keras.Sequential(
+            [
+                keras.Input(shape=(width,)),
+                layers.Dense(width, activation="relu"),
+                layers.Dense(width),
+            ],
+            name="projection_head",
+        )
+        # Single dense layer for linear probing
+        # Linear probing accuracy: It is computed as the accuracy of a logistic regression classifier trained on top of the encoder's features.
+        self.linear_probe = keras.Sequential(
+            [layers.Input(shape=(width,)), layers.Dense(10)], name="linear_probe"
+        )
+
+        self.encoder.summary()
+        self.projection_head.summary()
+        self.linear_probe.summary()
+
+    def compile(self, contrastive_optimizer, probe_optimizer, **kwargs):
+        super().compile(**kwargs)
+
+        self.contrastive_optimizer = contrastive_optimizer
+        self.probe_optimizer = probe_optimizer
+
+        # self.contrastive_loss will be defined as a method
+        # Contrastive accuracy: the ratio of cases in which the representation of an image is more similar to its differently augmented version's one than any other image.
+        self.probe_loss = keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+
+        self.contrastive_loss_tracker = keras.metrics.Mean(name="c_loss")
+        self.contrastive_accuracy = keras.metrics.SparseCategoricalAccuracy(
+            name="c_acc"
+        )
+        self.probe_loss_tracker = keras.metrics.Mean(name="p_loss")
+        self.probe_accuracy = keras.metrics.SparseCategoricalAccuracy(name="p_acc")
+
+    @property
+    def metrics(self):
+        return [
+            self.contrastive_loss_tracker,
+            self.contrastive_accuracy,
+            self.probe_loss_tracker,
+            self.probe_accuracy,
+        ]
+
+    def contrastive_loss(self, projections_1, projections_2):
+        # InfoNCE loss (information noise-contrastive estimation)
+        # NT-Xent loss (normalized temperature-scaled cross entropy)
+
+        # Cosine similarity: the dot product of the l2-normalized feature vectors
+        projections_1 = tf.math.l2_normalize(projections_1, axis=1)
+        projections_2 = tf.math.l2_normalize(projections_2, axis=1)
+        similarities = (
+            tf.matmul(projections_1, projections_2, transpose_b=True) / self.temperature
+        )
+
+        # The similarity between the representations of two augmented views of the
+        # same image should be higher than their similarity with other views
+        batch_size = tf.shape(projections_1)[0]
+        contrastive_labels = tf.range(batch_size)
+        self.contrastive_accuracy.update_state(contrastive_labels, similarities)
+        self.contrastive_accuracy.update_state(
+            contrastive_labels, tf.transpose(similarities)
+        )
+
+        # The temperature-scaled similarities are used as logits for cross-entropy
+        # a symmetrized version of the loss is used here
+        loss_1_2 = keras.losses.sparse_categorical_crossentropy(
+            contrastive_labels, similarities, from_logits=True
+        )
+        loss_2_1 = keras.losses.sparse_categorical_crossentropy(
+            contrastive_labels, tf.transpose(similarities), from_logits=True
+        )
+        return (loss_1_2 + loss_2_1) / 2
+
+    def train_step(self, data):
+        (unlabeled_images, _), (labeled_images, labels) = data
+
+        # Both labeled and unlabeled images are used, without labels
+        images = tf.concat((unlabeled_images, labeled_images), axis=0)
+        # Each image is augmented twice, differently
+        augmented_images_1 = self.contrastive_augmenter(images, training=True)
+        augmented_images_2 = self.contrastive_augmenter(images, training=True)
+        with tf.GradientTape() as tape:
+            features_1 = self.encoder(augmented_images_1, training=True)
+            features_2 = self.encoder(augmented_images_2, training=True)
+            # The representations are passed through a projection mlp
+            projections_1 = self.projection_head(features_1, training=True)
+            projections_2 = self.projection_head(features_2, training=True)
+            contrastive_loss = self.contrastive_loss(projections_1, projections_2)
+        gradients = tape.gradient(
+            contrastive_loss,
+            self.encoder.trainable_weights + self.projection_head.trainable_weights,
+        )
+        self.contrastive_optimizer.apply_gradients(
+            zip(
+                gradients,
+                self.encoder.trainable_weights + self.projection_head.trainable_weights,
+            )
+        )
+        self.contrastive_loss_tracker.update_state(contrastive_loss)
+
+        # Labels are only used in evalutation for an on-the-fly logistic regression
+        preprocessed_images = self.classification_augmenter(
+            labeled_images, training=True
+        )
+        with tf.GradientTape() as tape:
+            # the encoder is used in inference mode here to avoid regularization
+            # and updating the batch normalization paramers if they are used
+            features = self.encoder(preprocessed_images, training=False)
+            class_logits = self.linear_probe(features, training=True)
+            probe_loss = self.probe_loss(labels, class_logits)
+        gradients = tape.gradient(probe_loss, self.linear_probe.trainable_weights)
+        self.probe_optimizer.apply_gradients(
+            zip(gradients, self.linear_probe.trainable_weights)
+        )
+        self.probe_loss_tracker.update_state(probe_loss)
+        self.probe_accuracy.update_state(labels, class_logits)
+
+        return {m.name: m.result() for m in self.metrics}
+
+    def test_step(self, data):
+        labeled_images, labels = data
+
+        # For testing the components are used with a training=False flag
+        preprocessed_images = self.classification_augmenter(
+            labeled_images, training=False
+        )
+        features = self.encoder(preprocessed_images, training=False)
+        class_logits = self.linear_probe(features, training=False)
+        probe_loss = self.probe_loss(labels, class_logits)
+        self.probe_loss_tracker.update_state(probe_loss)
+        self.probe_accuracy.update_state(labels, class_logits)
+
+        # Only the probe metrics are logged at test time
+        return {m.name: m.result() for m in self.metrics[2:]}
+
+
+# Contrastive pretraining
+pretraining_model = ContrastiveModel()
+pretraining_model.compile(
+    contrastive_optimizer=keras.optimizers.Adam(),
+    probe_optimizer=keras.optimizers.Adam(),
+)
+
+pretraining_history = pretraining_model.fit(
+    train_dataset, epochs=num_epochs, validation_data=test_dataset
+)
+print(
+    "Maximal validation accuracy: {:.2f}%".format(
+        max(pretraining_history.history["val_p_acc"]) * 100
+    )
+)
+
+#################################################################################################################
+
+# Supervised finetuning of the pretrained encoder
+finetuning_model = keras.Sequential(
+    [
+        layers.Input(shape=(image_size, image_size, image_channels)),
+        get_augmenter(**classification_augmentation),
+        pretraining_model.encoder,
+        layers.Dense(10),
+    ],
+    name="finetuning_model",
+)
+finetuning_model.compile(
+    optimizer=keras.optimizers.Adam(),
+    loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+    metrics=[keras.metrics.SparseCategoricalAccuracy(name="acc")],
+)
+
+finetuning_history = finetuning_model.fit(
+    labeled_train_dataset, epochs=num_epochs, validation_data=test_dataset
+)
+print(
+    "Maximal validation accuracy: {:.2f}%".format(
+        max(finetuning_history.history["val_acc"]) * 100
+    )
+)
+################################################################################################################
+
+
+
+def get_eval_fn(finetuning_model):
+
+    # Load data and model here to avoid the overhead of doing it in `evaluate` itself
+    #(x_train, y_train), _ = tf.keras.datasets.cifar10.load_data()
+
+    # Use the last 5k training examples as a validation set
+    #x_val, y_val = x_train[45000:50000], y_train[45000:50000]
+
+
+    # The `evaluate` function will be called after every round
+    def evaluate(weights: fl.common.Weights) -> Optional[Tuple[float, Dict]]:
+        finetuning_model.set_weights(weights) 
+        loss, accuracy = finetuning_model.evaluate(test_dataset)
+        return loss, {"accuracy": accuracy}
+
+    return evaluate
+
+# Load and compile model for server-side parameter evaluation
+# model = tf.keras.applications.EfficientNetB0(
+#     input_shape=(32, 32, 3), weights=None, classes=10
+# )
+# model.compile("adam", "sparse_categorical_crossentropy", metrics=["accuracy"])
+
+
+# Create strategy
+strategy = fl.server.strategy.FedAvg(
+    # ... other FedAvg arguments
+    fraction_fit=1.0,
+    fraction_eval=1.0,
+    min_eval_clients=4,
+    min_available_clients=4,
+    eval_fn=get_eval_fn(finetuning_model),
+)
+
+# Start Flower server for four rounds of federated learning
+fl.server.start_server("localhost:3200",config={"num_rounds": 20},grpc_max_message_length=1024*1024*1024,strategy=strategy)
